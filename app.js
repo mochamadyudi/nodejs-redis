bbb44d263d7025172d2eb1d34bddcdf7c7eb9f3a
mochamadyudi <myudisobari12@gmail.com>
2022-04-06 01:45:22 +0700

    setup redis

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
 import express from 'express'
 import fetch from 'node-fetch'
+import "path";
 import redis from 'redis'
-// const express = require('express');
-// const fetch = require('node-fetch');
-// const redis = require('redis');
-
+import {RedisClient, RedisGet, RedisSetEx} from "./lib/plugins/redis.plugins.js";
+import BodyHandler from "./lib/handler/Body.handler.js";
+// RedisClient.
 const PORT = process.env.PORT || 5000;
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
-
-const client = redis.createClient(REDIS_PORT);
-
-
-function setResponse(username,repos){
-    return `<h2>${username} has ${repos} Github Repository</h2>`
-}
-
+const client = redis.createClient(REDIS_PORT,"127.0.0.1")
 const app = express();
+(async () => {
 
+
+    client.on('error', (err) => console.log('Redis Client Error', err));
+    await client.connect().then((r)=> console.log('connected!'));
+    //
+    // ('testing',3600, 'value');
+    // const value = await client.get('key');
+})();
 async function getRepos(req,res,next){
     try{
+
         console.log(`fetching data....`);
-        const {username}= req.params;
-        const response = await fetch(`https://api.github.com/users/${username}`)
+        const {id}= req.params;
+        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
 
         const data = await response.json();
 
-        const repos = data.public_repos;
+        await client.set(id,JSON.stringify({...data}));
+        // RedisSetEx(id,3600,repos);
 
-        client.setEx(username,3600,repos);
 
-
-        res.send(setResponse(username,repos));
+        res.status(200).json(new BodyHandler({id,data}));
     }catch(err){
         console.error(err);
         res.status(500).json({...err,error:true,message: err.message})
@@ -38,7 +39,45 @@ async function getRepos(req,res,next){
 }
 
 
-app.get('/repos/:username', getRepos);
+
+async function cache(req,res,next){
+    const {id} = req.params;
+
+    try{
+
+        const data = await client.get(id)
+        if(data){
+            res.status(200).json({error: false, status: 200, message: "Success Get in Redis", data : JSON.parse(data)});
+        }else{
+            next();
+        }
+
+        //     if (err) throw err;
+        //
+        //     console.log(id, "INI DIBAWAH")
+        //     res.status(200).json({error: false, status: 200, message: "Success Get in Redis", data});
+        }catch(err){
+        console.error(err)
+        next();
+    }
+
+
+    // await RedisClient.get(id, (err,data)=> {
+    //
+    //     console.log(err)
+    //     if(err) throw err;
+    //
+    //     console.log(id, "INI DIBAWAH")
+    //     if(data !== null){
+    //         res.status(200).json(new BodyHandler({error:false,status:200, message: "Success Get in Redis",data})) ;
+    //     }else{
+    //         next();
+    //     }
+    // })
+
+}
+
+app.get('/repos/:id', cache,getRepos);
 
 
 
